83fda3de56582a015c00d529a28b3465731396da
Huelerssey Rodrigues <111573894+Huelerssey@users.noreply.github.com>
2023-08-02 15:59:07 -0300

    update pg 1
    ajuste visual nas animacoes

--- pages/separador/pg_1_home.py
+++ pages/separador/pg_1_home.py
@@ -17,12 +17,12 @@ def home():
     
     # conteúdo a ser exibido na coluna 1
     with col1:
-        st_lottie(animacao_1, height=350, width=600)
+        st_lottie(animacao_1, height=350, width=400)
         st.write("")
         st.write("")
         st.write("")
         st.write("")
-        st.markdown("<h4 style='text-align: justfy;'> Ao longo do projeto, vou compartilhar os resultados formais, destacando o desempenho e a precisão do modelo, bem como sua aplicabilidade prática no auxílio ao diagnóstico dessa condição. Prepare-se para embarcar em uma jornada fascinante pelo mundo da ciência de dados e inteligência artificial!</h4>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: justfy;'> Ao longo do projeto, vou compartilhar os resultados formais, destacando o desempenho e a precisão do modelo, bem como sua aplicabilidade prática no auxílio ao diagnóstico dessa condição. Prepare-se para embarcar em uma jornada fascinante pelo mundo da ciência de dados e inteligência artificial!</h5>", unsafe_allow_html=True)
 
     # conteúdo a ser exibido na coluna 2
     with col2:
@@ -34,5 +34,5 @@ def home():
         st.write("")
         st.write("")
         st.write("")
-        st.markdown("<h4 style='text-align: justfy;'> Seja muito bem-vindo ao projeto de Previsão de Diabetes! Aqui, você terá a oportunidade de acompanhar todas as etapas envolvidas no desenvolvimento deste projeto, desde a concepção até a criação de um modelo de previsão personalizado, adaptado especificamente às características únicas de cada paciente.</h4>", unsafe_allow_html=True)
-        st_lottie(animacao_2, height=400, width=740)
+        st.markdown("<h5 style='text-align: justfy;'> Seja muito bem-vindo ao projeto de Previsão de Diabetes! Aqui, você terá a oportunidade de acompanhar todas as etapas envolvidas no desenvolvimento deste projeto, desde a concepção até a criação de um modelo de previsão personalizado, adaptado especificamente às características únicas de cada paciente.</h5>", unsafe_allow_html=True)
+        st_lottie(animacao_2, height=400, width=440)
